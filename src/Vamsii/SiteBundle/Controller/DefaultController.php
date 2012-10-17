7fa589938943f8be71a77015a02a3d1f83656fec
krish <plutonium.vamsi@gmail.com>
2012-10-17 20:51:10 +0530

    h2

--- src/Vamsii/SiteBundle/Controller/DefaultController.php
+++ src/Vamsii/SiteBundle/Controller/DefaultController.php
@@ -21,6 +21,19 @@ class DefaultController extends Controller
     				$hosted_dir = $this->container->getParameter('hosted_dir');
     				chdir($hosted_dir);
     				shell_exec("git pull");
+
+
+
+$message = \Swift_Message::newInstance()
+        ->setSubject('Deplouyed')
+        ->setFrom('vamsi@vamsii.com')
+        ->setTo('vamsi@vamsii.com')
+        ->setBody('Deployed')
+    ;
+    $this->get('mailer')->send($message);
+
+
+
     				$response = new Response();
     				$response->setContent("<html><body><h1>Deployed</h1></body></html>");
     				$response->setStatusCode(200);
