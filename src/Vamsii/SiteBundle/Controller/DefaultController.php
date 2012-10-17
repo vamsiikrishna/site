5c02ca05919c19ba02265805a1907ccf6211645a
krish <plutonium.vamsi@gmail.com>
2012-10-17 20:41:25 +0530

    Messing with Deploys

--- src/Vamsii/SiteBundle/Controller/DefaultController.php
+++ src/Vamsii/SiteBundle/Controller/DefaultController.php
@@ -3,11 +3,33 @@
 namespace Vamsii\SiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('VamsiiSiteBundle:Default:index.html.twig');
+    }
+
+    public function pullAction()
+    {
+    			$request = $this->getRequest();
+    			if ($request->getMethod() == 'POST') {
+
+    				$hosted_dir = $this->container->getParameter('hosted_dir');
+    				chdir($hosted_dir);
+    				shell_exec("git pull");
+    				$response = new Response();
+    				$response->setContent("<html><body><h1>Deployed</h1></body></html>");
+    				$response->setStatusCode(200);
+    				$response->headers->set('Content-Type', 'text/html');
+    				return $response;
+    			}
+    			else {
+    				return $this->render('VamsiiSiteBundle:Default:index.html.twig');
+
+    			}
     }
 }
